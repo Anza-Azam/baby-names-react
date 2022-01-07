780181b334707340cc5c614c373fa967021f1846
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2022-01-07 09:18:38 +0000

    level 4 complete

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import logo from "./logo.svg";
-import { useState} from "react"
-import Header from "./components/Header.js"
-import NameList from "./components/NameList.js"
-const names=require('./babyNamesData.json')
-import Selection from './components/Selection.js'
+
+import { useState } from "react";
+import Header from "./components/Header.js";
+import NameList from "./components/NameList.js";
+
+import Selection from "./components/Selection.js";
 import "./App.css";
 import Search from "./components/Search";
-import Favorites from './components/Favorites';
-
+import Favorites from "./components/Favorites";
+const names = require("./babyNamesData.json");
 function App() {
   const [namesList, setNamesList] = useState(names);
   const [newfavlist, setNewFavList] = useState([]);
-const [faveNameList, setFaveNameList] = useState([]);  
+  const [faveNameList, setFaveNameList] = useState([]);
   const handleInput = (searchresults) => {
-    setNamesList(searchresults)
- 
-  }
-
- 
-  const changeGender = (fav,fav2) => {
-    setNamesList(fav)
-    setFaveNameList(fav2)
-   
-  }
-  const handlefavnames=(fav) => {
-    setFaveNameList(faveNameList.concat(fav))
-    setNewFavList(newfavlist.concat(fav))
-   // let id = (namesList.findIndex(list => list.id === fav.id))
-   // let deleted= (namesList.splice(id, 1))
-    setNamesList([...namesList].filter(name=>name.id!==fav.id))
-
-  }
+    setNamesList(searchresults);
+  };
+  const changeGender = (fav, fav2) => {
+    setNamesList(fav);
+    setFaveNameList(fav2);
+  };
+  const handlefavnames = (fav) => {
+    setFaveNameList(faveNameList.concat(fav));
+    setNewFavList(newfavlist.concat(fav));
+    // let id = (namesList.findIndex(list => list.id === fav.id))
+    // let deleted= (namesList.splice(id, 1))
+    setNamesList([...namesList].filter((name) => name.id !== fav.id));
+  };
   const removeFavorite = (fav) => {
-     setNewFavList([...newfavlist].filter(name=>name.id!==fav.id));
-    setNamesList(namesList.filter(name=>name.id!==fav.id).concat(fav));
-    //let id = faveNameList.findIndex((list) => list.id === fav.id);
-    //let deleted = faveNameList.splice(id, 1);
-setFaveNameList([...faveNameList].filter(name=>name.id!==fav.id));
-  }
+    setNewFavList([...newfavlist].filter(name => name.id !== fav.id));
+    setNamesList(namesList.filter(name => name.id !== fav.id).concat(fav));
+    
+    setFaveNameList([...faveNameList].filter(name => name.id !== fav.id));
+  };
   if(!namesList)
         setNamesList(names)
   return (
